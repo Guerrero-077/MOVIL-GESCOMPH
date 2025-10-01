ec2201deba369a775230585365c0d843f163c74c
Guerrero-077 <brayansantiagoguerreromendez@gmail.com>
2025-10-01 13:59:48 -0500

    Fixed

--- GESCOMPH/capacitor.config.ts
+++ GESCOMPH/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'GESCOMPAH',
+  appName: 'GESCOMPH',
   webDir: 'www',
   plugins: {
     CapacitorHttp: { enabled: true },
